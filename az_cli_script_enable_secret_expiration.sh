3782aff4f214de8d9d97d8ea8f7a8cdba97ea646
Alejandro Guzmán <34655603+aaguzmanc@users.noreply.github.com>
2023-09-28 21:38:33 -0500

    Add files via upload

---
+++ az_cli_script_enable_secret_expiration.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Get KeyVault Names
+vlts=$(az keyvault list --output tsv --query "[].name")
+#Iterate each KeyVault
+for vl in $vlts
+do
+  echo -e "\nVault: $vl\n"
+#Get secrets
+    vsecs=$(az keyvault secret list --output tsv --query "[].name" --vault-name $vl)
+#Iterate Secrets
+	 for vs in $vsecs
+     do
+      echo -e "\nSecret: $vs\n"
+#Enable and set Expiration Date	  
+	  az keyvault secret set-attributes --expires "2025-10-30" --vault-name $vl -n $vs
+	  echo "\nExpiration set for: $vs\n"
+	 done
+done
