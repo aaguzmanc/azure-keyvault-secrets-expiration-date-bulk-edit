ff89e7907b1324d43f8f03d9df7f86a27b97cdfa
Alejandro Guzmán <34655603+aaguzmanc@users.noreply.github.com>
2023-10-02 11:45:08 -0500

    Update az_cli_script_validate_secret_expiration.sh

--- az_cli_script_validate_secret_expiration.sh
+++ az_cli_script_validate_secret_expiration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#SRIP TO VALIDATE EXPIRATION DATE ON KEVVAULT SECRETS - THIS IS A 
 #ObjectId for Service Principal or User signed in to run the script, it is necesary to grant access to the KeyVaylt secrets this user must have priviledges to manage KeyVaults
 uid=$(az ad signed-in-user show --query "[id]" --output tsv)
 
