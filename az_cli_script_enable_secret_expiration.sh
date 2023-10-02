38f339742887858edb5b1f44586701910d4c1ec1
Alejandro Guzmán <34655603+aaguzmanc@users.noreply.github.com>
2023-10-02 12:19:30 -0500

    Update az_cli_script_enable_secret_expiration.sh

--- az_cli_script_enable_secret_expiration.sh
+++ az_cli_script_enable_secret_expiration.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SRIPT TO SET EXPIRATION DATE ON KEVVAULT SECRETS MASSIVELY - By AG
-#THIS SCRIPT ADD THE REQUIERED PERMISSIONS TO MODIFY SECRETS AND THE REMOVE THOSE PERMISSIONS - ONLY NULL EXIRTION DATE WILL BE SELECTED
+#SECRETS EXPIRATION IS REQUIRED TO COMPLY WITH CIS AZURE FOUNDATIONS BENCHMARK V1.4 - CONTROLS 8.3 AND 8.4
+#THIS SCRIPT ADD THE REQUIRED PERMISSIONS TO MODIFY SECRETS AND THE REMOVE THOSE PERMISSIONS - ONLY NULL EXIRTION DATE WILL BE SELECTED
 #ObjectId for Service Principal or User signed in to run the script, it is necesary to grant access to the KeyVaylt secrets this user must have priviledges to manage KeyVaults
 uid=$(az ad signed-in-user show --query "[id]" --output tsv)
 
@@ -18,10 +19,10 @@ do
 	 for vs in $vsecs
      do
       echo -e "\nSecret: $vs\n"
-#Enable and set Expiration Date	  ********** BELOW YOU SHOULD SET YOUR DESIRED EXPIRATION DTAE FOR SECRETS - THIS WILL BE APPLIED TO ALL SECRETs MASSIVELY!
+#Enable and set Expiration Date	  ********** BELOW YOU SHOULD SET YOUR DESIRED EXPIRATION DTAE FOR SECRETS - THIS WILL BE APPLIED TO ALL SECRETS MASSIVELY!
 	  az keyvault secret set-attributes --expires "2025-10-30" --vault-name $vl -n $vs
 	  echo "\nExpiration set for: $vs\n"
 	 done
-# Remove Access to Vaul for User or Service Principal runninng this script	
+# Remove Access to Vault for User or Service Principal runninng this script	
 	az keyvault set-policy --name $vl --object-id $uid --secret-permissions --output table
 done
