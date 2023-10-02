729808282626ad2ac488942efeb7f545c8d15294
Alejandro Guzmán <34655603+aaguzmanc@users.noreply.github.com>
2023-10-02 09:38:24 -0500

    Add access control permission to access secrets

--- az_cli_script_validate_secret_expiration.sh
+++ az_cli_script_validate_secret_expiration.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+#ObjectId for Service Principal or User runninng the script, it is necesary to grant access to the KeyVaylt secrets this can be obtained 
+#from Azure AD - User - UserX - Object ID - paste the ID below:
+export uid="bxxxx-0000-0x00-00c0-6xxxxxxxx";
+
 #Get KeyVault Names
 vlts=$(az keyvault list --output tsv --query "[].name")
 #Iterate each KeyVault
 for vl in $vlts
 do
   echo -e "\nVault: $vl\n"
+  # Add Access to Vault for User or Service Principal runninng this script so he an modify Secrets
+	az keyvault set-policy --name $vl --object-id $uid --secret-permissions all --output table
 #Get secrets
     vsecs=$(az keyvault secret list --output tsv --query "[].name" --vault-name $vl)
 #Iterate Secrets
@@ -13,4 +21,6 @@ do
 #Display Secret Name and Expiration Date	 
 	  echo -e "\nExpiration: $expdate\n"
 	 done
+  # Remove Access to Vaul for User or Service Principal runninng this script	
+	az keyvault set-policy --name $vl --object-id $uid --secret-permissions --output table
 done
